db627b62c5de0886c486aacd74711674a55fd304
nellanap21 <ap543@student.london.ac.uk>
2025-06-15 15:59:39 -0700

    refactor createbooking to use middleware

--- public/organizer.js
+++ public/organizer.js
@@ -1,4 +1,8 @@
 // javascript functionality for the organizer home page
 
+function copyLink(event_id) {
 
-
+    const url = "http://localhost:3000/attendee/book-event/" + event_id;
+    navigator.clipboard.writeText(url);
+    alert("Link has been copied to clipboard!"); 
+}
--- routes/attendee.js
+++ routes/attendee.js
@@ -10,7 +10,7 @@ router.get("/", (req, res) => {
         siteDescription: req.app.locals.siteDescription
     };
 
-    let sqlquery = "SELECT * FROM events WHERE event_state = 1";
+    let sqlquery = "SELECT * FROM events WHERE event_state = 1 ORDER BY event_date ASC";
 
     global.db.all(sqlquery, 
         function (err, rows) {
@@ -38,57 +38,119 @@ router.get("/book-event/:id", (req, res) => {
     });
 });
 
-// sql query to check sold tickets
-// SELECT SUM(ticket_quantity) FROM bookings WHERE event_id = 1 AND ticket_type = 'general';
+// promisify db.all
+function dbAll(sql, params) {
+  return new Promise((resolve, reject) => {
+    db.all(sql, params, (err, rows) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(rows);
+      }
+    });
+  });
+}
 
-router.post("/book-event/:id/:ticketMax/:discountMax", (req, res) => {
+// promisify db.get
+function dbGet(sql, params) {
+  return new Promise((resolve, reject) => {
+    db.get(sql, params, (err, rows) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(rows);
+      }
+    });
+  });
+}
+
+// promisify db.run
+function dbRun(sql, params) {
+  return new Promise((resolve, reject) => {
+    db.run(sql, params, 
+        function (err) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve({ lastID: this.lastID, changes: this.changes });
+      }
+    });
+  });
+}
+
+async function getEvent (req, res, next) {
+    const recordId = req.params.id;
+    let sqlquery = "SELECT * FROM events WHERE event_id = ?";
+    try {
+        const result = await dbGet(sqlquery, recordId);
+        req.body.ticket_max = result.ticket_max;
+        req.body.d_ticket_max = result.d_ticket_max;
+        next();
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function checkTickets (req, res, next) {
     const eventId = req.params.id;
-    const ticketMax = req.params.ticketMax;
-    const discountMax = req.params.discountMax;
-
     let sqlqueryCheck = "SELECT * FROM bookings WHERE event_id = ? AND ticket_type = ?";
     let bookingCheckData = [eventId, req.body.ticket_type];
-    global.db.all(sqlqueryCheck, bookingCheckData,
-        function (err, rows) {
-            ticketsSold = 0;
-            for (let i = 0; i < rows.length; i++) {
-                ticketsSold += rows[i].ticket_quantity;
-            }
+    let rows = [];
+    try {
+        rows = await dbAll(sqlqueryCheck, bookingCheckData);
+    } catch (error) {
+        console.error(error);
+    }
+    // calculate tickets already sold
+    let ticketsSold = 0;
+    for (let i = 0; i < rows.length; i++) {
+        ticketsSold += rows[i].ticket_quantity;
+    }
 
-            if (req.body.ticket_type == 'general' && (ticketsSold + req.body.ticket_quantity) > ticketMax) {
-                res.send(`There are only ${ticketMax - ticketsSold} tickets available.`)
-                // change send to alert and redirect
-            }
-            if (req.body.ticket_type == 'discount' && (ticketsSold + req.body.ticket_quantity > discountMax)) {
-                res.send(`There are only ${discountMax - ticketsSold} tickets available.`);
-                // change send to alert and redirect
-            }  
+    if (req.body.ticket_type == 'general' && (ticketsSold + parseInt(req.body.ticket_quantity)) > req.body.ticket_max) {
+        res.send(`There are only ${req.body.ticket_max - ticketsSold} general tickets available.`)
+        // change send to alert and redirect
+    }
+    else if (req.body.ticket_type == 'discount' && (ticketsSold + parseInt(req.body.ticket_quantity)) > req.body.d_ticket_max) {
+        res.send(`There are only ${req.body.d_ticket_max - ticketsSold} discount tickets available.`);
+        // change send to alert and redirect
+    } else {
+        next();
+    }
+};
 
-            let sqlqueryAttendee = "INSERT INTO attendees ('attendee_name', 'attendee_email') VALUES (?, ?)";
-            let newAttendee = [req.body.attendee_name, req.body.attendee_email];
-            global.db.run(sqlqueryAttendee, newAttendee, 
-                function (err) {
-                    if (err) {
-                        next(err);
-                    } else {
-                        const attendeeId = this.lastID;
-                        const sqlqueryBooking = "INSERT INTO bookings ('ticket_type', 'ticket_quantity', 'event_id', 'attendee_id') VALUES (?, ?, ?, ?)";
-                        let newBooking = [req.body.ticket_type, req.body.ticket_quantity, eventId, attendeeId];
-                        global.db.run(sqlqueryBooking, newBooking, 
-                            function(err) {
-                                if (err) {
-                                    next(err);
-                                } else {
-                                    res.redirect("/attendee");
-                                }
-                            }
-                        )
-                    }
-                }
-            )
-        }
-        
-    )
+async function createAttendee (req, res, next) {
+
+    // TODO: Change so you check if attendee already exists
+
+    let sqlqueryAttendee = "INSERT INTO attendees ('attendee_name', 'attendee_email') VALUES (?, ?)";
+    let newAttendee = [req.body.attendee_name, req.body.attendee_email];
+    try {
+        result = await dbRun(sqlqueryAttendee, newAttendee);
+        req.body.attendeeId = result.lastID;
+        next();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function createBooking (req, res, next) {
+    const sqlqueryBooking = "INSERT INTO bookings ('ticket_type', 'ticket_quantity', 'event_id', 'attendee_id') VALUES (?, ?, ?, ?)";
+    let newBooking = [req.body.ticket_type, req.body.ticket_quantity, req.params.id, req.body.attendeeId];
+    try {
+        result = await dbRun(sqlqueryBooking, newBooking);
+        next();
+        // res.send("booking created")
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const booking = [getEvent, checkTickets, createAttendee, createBooking];
+
+
+router.post("/book-event/:id/", booking, (req, res) => {
+    res.redirect("/attendee");
 
 });
 

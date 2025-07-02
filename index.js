3014dcf22bd1968a0172d69a63ba6331761a6666
nellanap21 <ap543@student.london.ac.uk>
2025-07-01 19:11:28 -0700

    install bootstrap, configure express, and link css stylesheet

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 // Set up express, bodyparser and EJS
 const express = require('express');
 const session = require('express-session');
+const path = require('path');
+
 const app = express();
 const port = 3000;
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs'); // set the app to use ejs for rendering
 app.use(express.static(__dirname + '/public')); // set location of static files
+app.use('/bootstrap', express.static(path.join(__dirname, 'node_modules/bootstrap/dist')));
 
 // Set up express session
 app.use(session({
